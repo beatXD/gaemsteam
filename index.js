741f8a6b8507dc484119777535b7a92ea626c069
beat <panupongl3@hotmail.com>
2019-05-07 23:14:28 +0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import fs from 'fs';
 import game from './game';
 
-let arr = [];
-for (let i = 0; i < game.length; i += 1) {
-  arr = arr.concat(game[i].tags);
-}
-console.log(arr);
-// const newArr = [...new Set(arr)];
+let arrtags = [];
+let arrname = [];
+let arrlink = [];
+let arrtitle = [];
 
-// // // for(){}
-// const data = JSON.stringify(newArr, null, 2);
-// fs.writeFile('gametag/values.json', data, (err) => {
-//   if (err) throw err;
-// });
-// console.log(newArr);
+for (let i = 0; i < game.length; i += 1) {
+  arrtags = arrtags.concat(game[i].tags);
+  arrname = arrname.concat(game[i].name);
+  arrlink = arrlink.concat(game[i].link);
+  arrtitle = arrtitle.concat(game[i].title);
+}
+const newArr = [...new Set(arrtags)];
+
+console.log(newArr);
+
+for (let a = 0; a < newArr.length; a += 1) {
+  const data = JSON.stringify(newArr, null, 2);
+  fs.writeFile(`gametag/${newArr[a]}.json`, data, (err) => {
+    if (err) throw err;
+  });
+}
