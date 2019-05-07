b35ca7692d5ff23bee409a8c1ce4122992a22d1c
beat <panupongl3@hotmail.com>
2019-05-07 21:17:16 +0700

    update

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ let arr = [];
 for (let i = 0; i < game.length; i += 1) {
   arr = arr.concat(game[i].tags);
 }
-// console.log(arr);
-const newArr = [...new Set(arr)];
+console.log(arr);
+// const newArr = [...new Set(arr)];
 
-// // for(){}
-const data = JSON.stringify(newArr, null, 2);
-fs.writeFile('gametag/values.json', data, (err) => {
-  if (err) throw err;
-});
-console.log(newArr);
+// // // for(){}
+// const data = JSON.stringify(newArr, null, 2);
+// fs.writeFile('gametag/values.json', data, (err) => {
+//   if (err) throw err;
+// });
+// console.log(newArr);
