0a5dc45960c61bb50b62753407d014fa413373da
panupong <panupong043@gmail.com>
2019-05-07 18:03:10 +0700

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+import fs from 'fs';
+import game from './game';
+
+let arr = [];
+for (let i = 0; i < game.length; i += 1) {
+  arr = arr.concat(game[i].tags);
+}
+// console.log(arr);
+const newArr = [...new Set(arr)];
+
+// // for(){}
+const data = JSON.stringify(newArr, null, 2);
+fs.writeFile('gametag/values.json', data, (err) => {
+  if (err) throw err;
+});
+console.log(newArr);
