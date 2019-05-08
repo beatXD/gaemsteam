e9723db3a3d267d06106fe0e91e7688fede82812
panupong <panupong043@gmail.com>
2019-05-08 17:13:44 +0700

    success

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ import fs from 'fs';
 import game from './game';
 
 let arrtags = [];
+let newArrtags = [];
+let time = [];
+let data = [];
+let testData = [];
 let newData = [];
-//const date = [new Date().toLocaleString()];
 
 for (let i = 0; i < game.length; i += 1) {
   arrtags = arrtags.concat(game[i].tags);
 }
-const newArrtags = [...new Set(arrtags)];
+newArrtags = [...new Set(arrtags)];
 
 newArrtags.forEach((element) => {
-  newData = game.filter(user => user.tags.includes(element));
-
-  const time = JSON.stringify(date, null, 2);
- // const testData = { ...newData, date : time };
-  const data = JSON.stringify(newData, null, 2);
+  testData = game.filter(user => user.tags.includes(element));
+  time = JSON.stringify(new Date().toLocaleString(), null, 2);
+  newData = { ...testData, date: time };
+  data = JSON.stringify(newData, null, 2);
 
   fs.writeFile(`gametag/${element}.json`, data, (err) => {
     if (err) throw err;
   });
+});
