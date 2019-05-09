1ddc0d6473a6ca341f2ed1ac38266a04c1ed52ed
panupong <panupong043@gmail.com>
2019-05-09 14:03:45 +0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,75 @@
 import fs from 'fs';
-// import SteamAPI from 'steamapi';
 import game from './game';
 
-const arrtags = [];
+const tags = [];
 
-for (let i = 0; i < game.length; i += 1) {
-  arrtags = arrtags.concat(game[i].tags);
-}
-const newArrtags = [...new Set(arrtags)];
+const getid = (element) => {
+  const idsplit = (game.link);
+  const id = `${idsplit.split('/')[4]}`;
+  return id;
+};
 
-newArrtags.forEach((element) => {
-  const filter = game.filter(user => user.tags.includes(element));
-  const time = new Date().toLocaleString();
-  const result = { ...filter, Date: time };
-  const data = JSON.stringify(result, null, 2);
+const gethost = (element) => {
+  const hostsplit = (game.image);
+  const host = `${hostsplit.split('/')[3]}`;
+  return host;
+};
 
-  fs.writeFile(`gametag/${element}.json`, data, (err) => {
-    if (err) throw err;
+const getdiscount = (element) => {
+  const discountslit = (game.title);
+  const discount = `${discountslit.slice(5, 8)}`;
+  return discount;
+};
+
+const getupdatetime = (element) => {
+  const time = new Date().toISOString();
+  return time;
+};
+
+const geturl = (element) => game.link;
+const getname = (element) => game[1].name;
+const getimage = (element) => game.image;
+
+
+const creatobj = (element) => {
+  const gameobj = {
+    id: getid(element),
+    host: gethost(element),
+    url: geturl(element),
+    name: getname(element),
+    discount_percent: getdiscount(element),
+    image: getimage(element),
+    update_at: getupdatetime(element),
+
+  };
+  return gameobj;
+};
+console.log(creatobj());
+
+
+const appendjson (gamelistobj) => {
+  const data = JSON.stringify(gamelistobj, null, 2);
+  fs.writeFile(`gametag/${tags}.json`, data, (err) => {
+   if (err) throw err;
+ });
+});
+
+
+
+game.forEach((element) => {
+  const gamelistobj = creatobj(element);
+element.tags.forEach(tags) => {
+  appendjson (gamelistobj,tags);
   });
 });
+
+// for (let i = 0; i < game.length; i += 1) {
+//   tags = tags.concat(game[i].tags);
+// }
+// const newArrtags = [...new Set(tags)];
+
+// newArrtags.forEach((element) => {
+//   const filter = game.filter(user => user.tags.includes(element));
+//   const result = { ID: id, Date: time, ...filter };
+//   const data = JSON.stringify(result, null, 2);
+
