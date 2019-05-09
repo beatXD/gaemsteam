53aeb1fad2ff85f9aa89e8d9d67d9668e21218b0
panupong <panupong043@gmail.com>
2019-05-09 14:38:41 +0700

    submit

--- index.js
+++ index.js
@@ -1,70 +1,56 @@
 import fs from 'fs';
-import game from './game';
+import gameJSON from './game';
 
-const tags = [];
-let gamelistobj;
-
-const getid = (element) => {
-  const idsplit = (game.link);
+const getid = (game) => {
+  const idsplit = game.link;
   const id = `${idsplit.split('/')[4]}`;
   return id;
 };
 
-const gethost = (element) => {
-  const hostsplit = (game.image);
+const gethost = (game) => {
+  const hostsplit = game.image;
   const host = `${hostsplit.split('/')[3]}`;
   return host;
 };
 
-const getdiscount = (element) => {
-  const discountslit = (game.title);
-  const discount = `${discountslit.slice(5, 8)}`;
+const getdiscount = (game) => {
+  const discountslit = game.title;
+  const discount = `${discountslit.slice(5, 7)}`;
   return discount;
 };
 
-const getupdatetime = (element) => {
+const getupdatetime = () => {
   const time = new Date().toISOString();
   return time;
 };
 
-const geturl = element => game.link;
-const getname = element => game[1].name;
-const getimage = element => game.image;
+const geturl = game => game.link;
+const getname = game => game.name;
+const getimage = game => game.image;
 
 
-const creatobj = (element) => {
+const creatobj = (game) => {
   const gameobj = {
-    id: getid(element),
-    host: gethost(element),
-    url: geturl(element),
-    name: getname(element),
-    discount_percent: getdiscount(element),
-    image: getimage(element),
-    update_at: getupdatetime(element),
-
+    id: getid(game),
+    host: gethost(game),
+    url: geturl(game),
+    name: getname(game),
+    discount_percent: getdiscount(game),
+    image: getimage(game),
+    update_at: getupdatetime(),
   };
   return gameobj;
 };
-console.log(creatobj());
+// console.log(creatobj());
 
+gameJSON.forEach((game) => {
+  const gamelist = creatobj(game);
 
+  game.tags.forEach((tags) => {
+    const data = JSON.stringify(gamelist, null, 2);
 
-game.forEach((element) => {
-  const gamelist = creatobj(element);
-
-element.tags.forEach((tags) => {
-  const data = JSON.stringify(gamelist, null, 2);
-  fs.writeFile(`gametag/${tags}.json`, data, (err) => { if (err) throw err; 
+    fs.writeFileSync(`gametag/${tags}.json`, data, (err) => {
+      if (err) throw err;
+    });
   });
 });
-
-
-// for (let i = 0; i < game.length; i += 1) {
-//   tags = tags.concat(game[i].tags);
-// }
-// const newArrtags = [...new Set(tags)];
-
-// newArrtags.forEach((element) => {
-//   const filter = game.filter(user => user.tags.includes(element));
-//   const result = { ID: id, Date: time, ...filter };
-//   const data = JSON.stringify(result, null, 2);
